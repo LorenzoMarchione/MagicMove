7f985fcb404e5cc40c7cc3e92f40b64fd0fe5d62
LorenzoMarchione <lorenmarchione@gmail.com>
2026-07-13 12:49:28 -0300

    feat: slide state
    also changed some jump logic and other small adjustments

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -9,18 +10,22 @@ public class Player : MonoBehaviour
     //lateral movement input
     private Vector2 move;
     private bool sprint = false;
+    private bool crouching = false;
     private bool jumpPressed = false;
     private bool jumpReleased = false;
 
     private float facing = 1;
 
     //check variables
-    public bool isGrounded;
+    private bool isGrounded;
 
     [Header("Movement settings")]
     [SerializeField] private float walkSpeed;
     [SerializeField] private float runSpeed;
     [SerializeField] private float jumpForce;
+
+    [Header("Jump settings")]
+    [SerializeField] private float jumpWindowDuration;
 
     [Header("Check settings")]
     [SerializeField] private float groundCheckLength;
@@ -31,6 +36,13 @@ public class Player : MonoBehaviour
     [SerializeField] private float upGravity;
     [SerializeField] private float downGravity;
     [SerializeField] private float normalGravity;
+
+    [Header("Slide settings")]
+    [SerializeField] private float slideDuration;
+    [SerializeField] private float slideLockDuration;
+    [SerializeField] private float slideSpeed;
+    private bool sliding = false;
+    private bool slideLock = false;
 
     [Header("Layers")]
     [SerializeField] private LayerMask floor;
@@ -55,11 +67,15 @@ public class Player : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        Movement();
-        Jump();
+        CrouchSlide();
+        if (!sliding)
+        {
+            Movement();
+            Jump();
+        }
         ApplyVariableGravity();
     }
-    //move player based on input and flip to face move direction
+    //move player based on move input and sprint input, also flip to face move direction
     private void Movement()
     {
         float targetSpeed = sprint? runSpeed : walkSpeed;
@@ -72,7 +88,7 @@ public class Player : MonoBehaviour
     //jump physics
     private void Jump()
     {
-        if (jumpPressed && isGrounded)
+        if (jumpPressed && isGrounded && !crouching)
         {
             jumpPressed = false;
             rb.linearVelocityY = 0;
@@ -83,47 +99,68 @@ public class Player : MonoBehaviour
         
         jumpReleased = false;
     }
+    //start sliding state 
+    private void CrouchSlide()
+    {
+        if(crouching && jumpPressed && !sliding && !slideLock)
+        {
+            StartCoroutine(Slide());
+            jumpPressed = false;
+        }
+        else if(sliding)
+            rb.linearVelocityX = facing * slideSpeed;
+    }
+    //all animation checks
     private void AnimStates()
     {
-        if (isGrounded && Mathf.Abs(rb.linearVelocityX) > 0.1)
-            anim.SetBool("isRunning", true);
-        else
-            anim.SetBool("isRunning", false);
-
-        if (!isGrounded && rb.linearVelocityY > 0.1)
-            anim.SetBool("isJumping", true);
-        else
-            anim.SetBool("isJumping", false);
-
-        if (!isGrounded && rb.linearVelocityY < -0.1)
-            anim.SetBool("isFalling", true);
-        else
-            anim.SetBool("isFalling", false);
-
-        if (isGrounded && Mathf.Abs(rb.linearVelocityX) < 0.1)
-            anim.SetBool("isIdle", true);
-        else
-            anim.SetBool("isIdle", false);
+        anim.SetBool("isRunning", isGrounded && Mathf.Abs(rb.linearVelocityX) > 0.1 && !crouching);
+        anim.SetBool("isJumping", !isGrounded && rb.linearVelocityY > 0.1);
+        anim.SetBool("isFalling", !isGrounded && rb.linearVelocityY < -0.1);
+        anim.SetBool("isIdle", isGrounded && Mathf.Abs(rb.linearVelocityX) < 0.1 && !crouching);
+        anim.SetBool("isCrouching", isGrounded && crouching && !sliding);
+        anim.SetBool("isSliding", isGrounded && sliding);
     }
-    //save only x axis from move controls
+    //move input and crouch check
     private void OnMove(InputValue input)
     {
         move = input.Get<Vector2>(); 
+        crouching = move.y < -0.1;
     }
     //jump input logic
     private void OnJump(InputValue input)
     {
         if (input.isPressed)
+        {
             jumpPressed = input.isPressed;
+            StartCoroutine(JumpWindow());
+        }
         else
             jumpReleased = true;
     }
+    //sprint button
     private void OnSprint(InputValue input)
     {
         if (input.isPressed)
             sprint = true;
         else
             sprint = false;
+    }
+    //coroutine to allow jumping with slight earlier input
+    private IEnumerator JumpWindow()
+    {
+        yield return new WaitForSeconds(jumpWindowDuration);
+        if(jumpPressed)
+            jumpPressed = false;
+    }
+    //coroutine controlling slide timers
+    private IEnumerator Slide()
+    {
+        sliding = true;
+        yield return new WaitForSeconds(slideDuration);
+        sliding = false;
+        slideLock = true;
+        yield return new WaitForSeconds(slideLockDuration);
+        slideLock = false;
     }
     //make player face opposite direction
     private void FLip()
